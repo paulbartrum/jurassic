06734faac89cce7007fd36ef87e38e260a946886
paulbartrum <paulbartrum@localhost>
2010-08-16 05:11:54 +0000

    Split off code common to sunspider and v8 benchmark tests.

--- Performance/SunSpider.cs
+++ Performance/SunSpider.cs
@@ -7,233 +7,175 @@ namespace Performance
 {
 
     /// <summary>
-    /// Test the Parser object.
+    /// Run the SunSpider performance tests.
     /// </summary>
     [TestClass]
     public class SunSpider
     {
-        [ThreadStatic]
-        private static Jurassic.ScriptEngine engine;
-
-        [ThreadStatic]
-        private static System.Diagnostics.Stopwatch timer;
-
-        [ThreadStatic]
-        private static double parseTime, optimizationTime, codeGenerationTime;
+        private static ScriptEngineManager manager;
 
         [ClassInitialize]
         public static void WarmUp(TestContext context)
         {
-            // Create a new script engine.
-            var warmUpTimer = System.Diagnostics.Stopwatch.StartNew();
-            engine = new Jurassic.ScriptEngine();
-            context.WriteLine("Warm up time: {0:n2}ms", warmUpTimer.Elapsed.TotalMilliseconds);
-
-            // Hook events to record statistics.
-            engine.OptimizationStarted += (sender, e) => { parseTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
-            engine.CodeGenerationStarted += (sender, e) => { optimizationTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
-            engine.ExecutionStarted += (sender, e) => { codeGenerationTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
+            manager = new ScriptEngineManager(@"..\..\..\Performance\Files\sunspider-0.9.1\");
         }
 
         [TestMethod]
         public void threed_cube()
         {
-            RunTest(@"3d-cube.js", 217.5);
+            manager.RunTest(@"3d-cube.js", 217.5);
         }
 
         [TestMethod]
         public void threed_morph()
         {
-            RunTest(@"3d-morph.js", 596.1);
+            manager.RunTest(@"3d-morph.js", 596.1);
         }
 
         [TestMethod]
         public void threed_raytrace()
         {
-            RunTest(@"3d-raytrace.js", 597.9);
+            manager.RunTest(@"3d-raytrace.js", 597.9);
         }
 
         [TestMethod]
         public void access_binary_trees()
         {
-            RunTest(@"access-binary-trees.js", 1152.5);
+            manager.RunTest(@"access-binary-trees.js", 1152.5);
         }
 
         [TestMethod]
         public void access_fannkuch()
         {
-            RunTest(@"access-fannkuch.js", 1575.7);
+            manager.RunTest(@"access-fannkuch.js", 1575.7);
         }
 
         [TestMethod]
         public void access_nbody()
         {
-            RunTest(@"access-nbody.js", 718.9);
+            manager.RunTest(@"access-nbody.js", 718.9);
         }
 
         [TestMethod]
         public void access_nsieve()
         {
-            RunTest(@"access-nsieve.js", 617.6);
+            manager.RunTest(@"access-nsieve.js", 617.6);
         }
 
         [TestMethod]
         public void bitops_3bit_bits_in_byte()
         {
-            RunTest(@"bitops-3bit-bits-in-byte.js", 161.6);
+            manager.RunTest(@"bitops-3bit-bits-in-byte.js", 161.6);
         }
 
         [TestMethod]
         public void bitops_bits_in_byte()
         {
-            RunTest(@"bitops-bits-in-byte.js", 202.9);
+            manager.RunTest(@"bitops-bits-in-byte.js", 202.9);
         }
 
         [TestMethod]
         public void bitops_bitwise_and()
         {
-            RunTest(@"bitops-bitwise-and.js", 1082.8);
+            manager.RunTest(@"bitops-bitwise-and.js", 1082.8);
         }
 
         [TestMethod]
         public void bitops_nsieve_bits()
         {
-            RunTest(@"bitops-nsieve-bits.js", 617.9);
+            manager.RunTest(@"bitops-nsieve-bits.js", 617.9);
         }
 
         [TestMethod]
         public void controlflow_recursive()
         {
-            RunTest(@"controlflow-recursive.js", 162.8);
+            manager.RunTest(@"controlflow-recursive.js", 162.8);
         }
 
         [TestMethod]
         public void crypto_aes()
         {
-            RunTest(@"crypto-aes.js", 967.6);
+            manager.RunTest(@"crypto-aes.js", 967.6);
         }
 
         [TestMethod]
         public void crypto_md5()
         {
-            RunTest(@"crypto-md5.js", 784.0);
+            manager.RunTest(@"crypto-md5.js", 784.0);
         }
 
         [TestMethod]
         public void crypto_sha1()
         {
-            RunTest(@"crypto-sha1.js", 411.2);
+            manager.RunTest(@"crypto-sha1.js", 411.2);
         }
 
         [TestMethod]
         public void date_format_tofte()
         {
-            RunTest(@"date-format-tofte.js", 660.3);
+            manager.RunTest(@"date-format-tofte.js", 660.3);
         }
 
         [TestMethod]
         public void date_format_xparb()
         {
-            RunTest(@"date-format-xparb.js", 583.3);
+            manager.RunTest(@"date-format-xparb.js", 583.3);
         }
 
         [TestMethod]
         public void math_cordic()
         {
-            RunTest(@"math-cordic.js", 530.7);
+            manager.RunTest(@"math-cordic.js", 530.7);
         }
 
         [TestMethod]
         public void math_partial_sums()
         {
-            RunTest(@"math-partial-sums.js", 514.6);
+            manager.RunTest(@"math-partial-sums.js", 514.6);
         }
 
         [TestMethod]
         public void math_spectral_norm()
         {
-            RunTest(@"math-spectral-norm.js", 335.2);
+            manager.RunTest(@"math-spectral-norm.js", 335.2);
         }
 
         [TestMethod]
         public void regexp_dna()
         {
-            RunTest(@"regexp-dna.js", 942.6);
+            manager.RunTest(@"regexp-dna.js", 942.6);
         }
 
         [TestMethod]
         public void string_base64()
         {
-            RunTest(@"string-base64.js", 2310.8);
+            manager.RunTest(@"string-base64.js", 2310.8);
         }
 
         [TestMethod]
         public void string_fasta()
         {
-            RunTest(@"string-fasta.js", 1005.8);
+            manager.RunTest(@"string-fasta.js", 1005.8);
         }
 
         [TestMethod]
         public void string_tagcloud()
         {
-            RunTest(@"string-tagcloud.js", 2268.3);
+            manager.RunTest(@"string-tagcloud.js", 2268.3);
         }
 
         [TestMethod]
         public void string_unpack_code()
         {
-            RunTest(@"string-unpack-code.js", 3479.1);
+            manager.RunTest(@"string-unpack-code.js", 3479.1);
         }
 
         [TestMethod]
         public void string_validate_input()
         {
-            RunTest(@"string-validate-input.js", 5343.1);   // 4500-6000
+            manager.RunTest(@"string-validate-input.js", 5343.1);   // 4500-6000
         }
 
-        //[TestMethod]
-        //public void RunAllTests()
-        //{
-        //    var timer = System.Diagnostics.Stopwatch.StartNew();
-        //    foreach (string path in Directory.EnumerateFiles(@"..\..\..\Performance\Files\sunspider-0.9.1"))
-        //        RunTest(path, 0, false);
-        //    Assert.Inconclusive(string.Format("{0}ms, was 24471ms", timer.ElapsedMilliseconds));
-        //}
-
-        private void RunTest(string scriptPath, double previous, bool assertResults = true)
-        {
-            scriptPath = Path.Combine(@"..\..\..\Performance\Files\sunspider-0.9.1\", scriptPath);
-            var script = File.ReadAllText(scriptPath);
-
-            // Execute the javascript code.
-            timer = System.Diagnostics.Stopwatch.StartNew();
-            engine.Execute(new Jurassic.StringScriptSource(script));
-            double runTime = timer.Elapsed.TotalMilliseconds;
-
-            string infoString = string.Format("{0:n1}ms (parse: {1:n1}ms, compile: {2:n1}ms, optimize: {3:n1}ms, runtime: {4:n1}ms)",
-                parseTime + optimizationTime + codeGenerationTime + runTime,
-                parseTime,
-                codeGenerationTime,
-                optimizationTime,
-                runTime);
-            if (previous > 0)
-                infoString += string.Format(", was {0:n1}ms", previous);
-            //Console.WriteLine("{0}: {1}", Path.GetFileNameWithoutExtension(scriptPath), infoString);
-            Console.WriteLine("{0}\t{1:n1}\t{2:n1}\t{3:n1}\t{4:n1}\t{5:n1}",
-                Path.GetFileNameWithoutExtension(scriptPath),
-                parseTime,
-                codeGenerationTime,
-                optimizationTime,
-                runTime,
-                parseTime + optimizationTime + codeGenerationTime + runTime);
-            if (assertResults == true)
-                throw new AssertInconclusiveException(infoString);
-
-            //var engine = new Jint.JintEngine();
-            //var timer = System.Diagnostics.Stopwatch.StartNew();
-            //engine.Run(script);
-            //throw new AssertInconclusiveException(string.Format("{0:n1}ms", timer.Elapsed.TotalMilliseconds));
-        }
     }
 
 }
--- Performance/v8.cs
+++ Performance/v8.cs
@@ -7,155 +7,87 @@ namespace Performance
 {
 
     /// <summary>
-    /// Test the Parser object.
+    /// Run the v8 performance tests.
     /// </summary>
     [TestClass]
     public class v8
     {
-        [ThreadStatic]
-        private static Jurassic.ScriptEngine engine;
-
-        [ThreadStatic]
-        private static System.Diagnostics.Stopwatch timer;
-
-        [ThreadStatic]
-        private static double parseTime, optimizationTime, codeGenerationTime;
-
-        private static object originalRandom;
+        private static ScriptEngineManager manager;
 
         [ClassInitialize]
         public static void WarmUp(TestContext context)
         {
-            // Create a new script engine.
-            var warmUpTimer = System.Diagnostics.Stopwatch.StartNew();
-            engine = new Jurassic.ScriptEngine();
-            context.WriteLine("Warm up time: {0:n2}ms", warmUpTimer.Elapsed.TotalMilliseconds);
+            manager = new ScriptEngineManager(@"..\..\..\Performance\Files\v8\", (engine) =>
+            {
 
-            // Hook events to record statistics.
-            engine.OptimizationStarted += (sender, e) => { parseTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
-            engine.CodeGenerationStarted += (sender, e) => { optimizationTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
-            engine.ExecutionStarted += (sender, e) => { codeGenerationTime = timer.Elapsed.TotalMilliseconds; timer.Restart(); };
+                // Replace the default random number generator with a deterministic one.
+                engine.Math["random"] = engine.Evaluate(@"
+                    (function() {
+                      var seed = 49734321;
+                      return function() {
+                        // Robert Jenkins' 32 bit integer hash function.
+                        seed = ((seed + 0x7ed55d16) + (seed << 12))  & 0xffffffff;
+                        seed = ((seed ^ 0xc761c23c) ^ (seed >>> 19)) & 0xffffffff;
+                        seed = ((seed + 0x165667b1) + (seed << 5))   & 0xffffffff;
+                        seed = ((seed + 0xd3a2646c) ^ (seed << 9))   & 0xffffffff;
+                        seed = ((seed + 0xfd7046c5) + (seed << 3))   & 0xffffffff;
+                        seed = ((seed ^ 0xb55a4f09) ^ (seed >>> 16)) & 0xffffffff;
+                        return (seed & 0xfffffff) / 0x10000000;
+                      };
+                    })();");
 
-            // Replace the default random number generator with a deterministic one.
-            originalRandom = engine.Math["random"];
-            engine.Math["random"] = engine.Evaluate(@"
-                (function() {
-                  var seed = 49734321;
-                  return function() {
-                    // Robert Jenkins' 32 bit integer hash function.
-                    seed = ((seed + 0x7ed55d16) + (seed << 12))  & 0xffffffff;
-                    seed = ((seed ^ 0xc761c23c) ^ (seed >>> 19)) & 0xffffffff;
-                    seed = ((seed + 0x165667b1) + (seed << 5))   & 0xffffffff;
-                    seed = ((seed + 0xd3a2646c) ^ (seed << 9))   & 0xffffffff;
-                    seed = ((seed + 0xfd7046c5) + (seed << 3))   & 0xffffffff;
-                    seed = ((seed ^ 0xb55a4f09) ^ (seed >>> 16)) & 0xffffffff;
-                    return (seed & 0xfffffff) / 0x10000000;
-                  };
-                })();");
-        }
+                // Some of the tests call alert if there is an error.  Since alert is not supported,
+                // translate this into throwing an exception.
+                engine.Global["alert"] = engine.Evaluate("(function alert(str) { throw new Error(str) })");
 
-        [ClassCleanup]
-        public static void CleanUp()
-        {
-            // Revert the random number generator.
-            engine.Math["random"] = originalRandom;
+                // For debugging purposes, define the console object.
+                engine.Global["console"] = new Jurassic.Library.FirebugConsole(engine);
+
+            });
         }
 
         [TestMethod]
         public void crypto()
         {
-            // The test calls alert if there is an error.  Since alert is not supported,
-            // translate this into throwing an exception.
-            engine.Global["alert"] = engine.Evaluate("(function alert(str) { throw new Error(str) })");
-
-            RunTest(@"crypto.js", 0);
+            manager.RunTest("crypto.js", 0);
         }
 
         [TestMethod]
         public void deltablue()
         {
-            // The test calls alert if there is an error.  Since alert is not supported,
-            // translate this into throwing an exception.
-            engine.Global["alert"] = engine.Evaluate("(function alert(str) { throw new Error(str) })");
-            
-            // Run the deltablue test.
-            RunTest(@"deltablue.js", 352.3);
+            manager.RunTest("deltablue.js", 352.3);
         }
 
         [TestMethod]
         public void earley_boyer()
         {
-            // The test calls alert if there is an error.  Since alert is not supported,
-            // translate this into throwing an exception.
-            engine.Global["alert"] = engine.Evaluate("(function alert(str) { throw new Error(str) })");
-
-            RunTest(@"earley-boyer.js", 0);
+            manager.RunTest("earley-boyer.js", 0);
         }
 
         [TestMethod]
         public void raytrace()
         {
-            RunTest(@"raytrace.js", 2345);
+            manager.RunTest("raytrace.js", 2345);
         }
 
         [TestMethod]
         public void regexp()
         {
-            RunTest(@"regexp.js", 3294);
+            manager.RunTest("regexp.js", 3294);
         }
 
         [TestMethod]
         public void richards()
         {
-            engine.Global["console"] = new Jurassic.Library.FirebugConsole(engine);
-            RunTest(@"richards.js", 298.7);
+            manager.RunTest("richards.js", 298.7);
         }
 
         [TestMethod]
         public void splay()
         {
-            engine.Global["console"] = new Jurassic.Library.FirebugConsole(engine);
-            RunTest(@"splay.js", 6873);
+            manager.RunTest(@"splay.js", 6873);
         }
 
-        [TestMethod]
-        public void RunAllTests()
-        {
-            var timer = System.Diagnostics.Stopwatch.StartNew();
-            foreach (string path in Directory.EnumerateFiles(@"..\..\..\Performance\Files\v8"))
-                RunTest(path, 0, false);
-            Assert.Inconclusive(string.Format("{0}ms, was 24471ms", timer.ElapsedMilliseconds));
-        }
-
-        private void RunTest(string scriptPath, double previous, bool assertResults = true)
-        {
-            scriptPath = Path.Combine(@"..\..\..\Performance\Files\v8\", scriptPath);
-            var script = File.ReadAllText(scriptPath);
-
-            // Execute the javascript code.
-            timer = System.Diagnostics.Stopwatch.StartNew();
-            engine.Execute(new Jurassic.StringScriptSource(script));
-            double runTime = timer.Elapsed.TotalMilliseconds;
-
-            string infoString = string.Format("{0:n1}ms (parse: {1:n1}ms, compile: {2:n1}ms, optimize: {3:n1}ms, runtime: {4:n1}ms)",
-                parseTime + optimizationTime + codeGenerationTime + runTime,
-                parseTime,
-                codeGenerationTime,
-                optimizationTime,
-                runTime);
-            if (previous > 0)
-                infoString += string.Format(", was {0:n1}ms", previous);
-            //Console.WriteLine("{0}: {1}", Path.GetFileNameWithoutExtension(scriptPath), infoString);
-            Console.WriteLine("{0}\t{1:n1}\t{2:n1}\t{3:n1}\t{4:n1}\t{5:n1}",
-                Path.GetFileNameWithoutExtension(scriptPath),
-                parseTime,
-                codeGenerationTime,
-                optimizationTime,
-                runTime,
-                parseTime + optimizationTime + codeGenerationTime + runTime);
-            if (assertResults == true)
-                throw new AssertInconclusiveException(infoString);
-        }
     }
 
 }
