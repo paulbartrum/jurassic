446c13ef2b6331e1ce2babf23dd0e74b514f303c
paulbartrum <paulbartrum@users.noreply.github.com>
2010-10-12 04:22:36 +0000

    Updated assembly attributes.

--- JurassicSL/Properties/AssemblyInfo.cs
+++ JurassicSL/Properties/AssemblyInfo.cs
@@ -5,22 +5,14 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("JurassicSL")]
+[assembly: AssemblyTitle("Jurassic for Silverlight")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JurassicSL")]
-[assembly: AssemblyCopyright("Copyright ©  2010")]
+[assembly: AssemblyCompany("Paul Bartrum")]
+[assembly: AssemblyProduct("Jurassic for Silverlight")]
+[assembly: AssemblyCopyright("Copyright © Paul Bartrum 2010")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
-
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1a0ef903-02db-4459-ad70-454823e643fc")]
+[assembly: AssemblyCulture("en-us")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,5 +23,5 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("2.0.*")]
+[assembly: AssemblyFileVersion("2.0.*")]
