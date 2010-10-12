d92e156427c9bc6a9da37dc0274473408f722898
paulbartrum <paulbartrum@localhost>
2010-10-12 04:22:36 +0000

    Updated assembly attributes.

--- Jurassic/Properties/AssemblyInfo.cs
+++ Jurassic/Properties/AssemblyInfo.cs
@@ -12,15 +12,12 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Jurassic")]
 [assembly: AssemblyCopyright("Copyright © Paul Bartrum 2010")]
 [assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+[assembly: AssemblyCulture("en-us")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("dc3e09ae-fbce-48f7-a83d-efef62257918")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,8 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.*")]
+[assembly: AssemblyVersion("2.0.*")]
 
 // Unit tests and performance tests need access to internal members.
 [assembly: InternalsVisibleTo("Unit Tests")]
