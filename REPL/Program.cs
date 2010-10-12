6d2f85baef6750e7165deca6e4ed9672cdce1fe9
paulbartrum <paulbartrum@users.noreply.github.com>
2010-10-12 14:04:28 +0000

    REPL now waits until a complete command has been entered.

--- REPL/Program.cs
+++ REPL/Program.cs
@@ -19,31 +19,57 @@ namespace REPL
             Console.WriteLine("JavaScript console (type 'quit' to exit)");
             Console.WriteLine("Most Firebug console commands are available");
             Console.WriteLine("(see http://getfirebug.com/wiki/index.php/Console_API)");
+
             while (true)
             {
-                Console.WriteLine();
-                Console.Write("> ");
-                string source = Console.ReadLine();
-                if (source == "exit" || source == "quit")
-                    return;
-                try
+                var source = new StringBuilder();
+                var continuation = false;
+                do
                 {
-                    var result = engine.Evaluate(source);
-
-                    // Write the result.
-                    var original = Console.ForegroundColor;
-                    if (result == Jurassic.Undefined.Value)
-                        Console.ForegroundColor = ConsoleColor.Gray;
+                    if (continuation == false)
+                    {
+                        Console.WriteLine();
+                        Console.Write("> ");
+                        string line = Console.ReadLine();
+                        if (line == "exit" || line == "quit")
+                            return;
+                        source.Append(line);
+                    }
                     else
-                        Console.ForegroundColor = ConsoleColor.White;
-                    Console.Write("-> ");
-                    Console.WriteLine(Jurassic.TypeConverter.ToString(result));
-                    Console.ForegroundColor = original;
-                }
-                catch (Exception ex)
-                {
-                    console.Error(ex.Message);
-                }
+                    {
+                        // This is a continuation of a previous command.
+                        Console.Write("  ");
+                        source.Append(Environment.NewLine);
+                        source.Append(Console.ReadLine());
+                    }
+
+                    // Check for an empty command.
+                    if (source.Length == 0)
+                        break;
+
+                    try
+                    {
+                        continuation = false;
+                        var result = engine.Evaluate(source.ToString());
+
+                        // Write the result.
+                        var original = Console.ForegroundColor;
+                        if (result == Jurassic.Undefined.Value)
+                            Console.ForegroundColor = ConsoleColor.Gray;
+                        else
+                            Console.ForegroundColor = ConsoleColor.White;
+                        Console.Write("-> ");
+                        Console.WriteLine(Jurassic.TypeConverter.ToString(result));
+                        Console.ForegroundColor = original;
+                    }
+                    catch (Exception ex)
+                    {
+                        if (ex.Message.Contains("end of input"))
+                            continuation = true;
+                        else
+                            console.Error(ex.Message);
+                    }
+                } while (continuation == true);
             }
         }
     }
