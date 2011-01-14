78dabc76b2a675a0574461269c51b44096d5938b
paulbartrum <paulbartrum@localhost>
2011-01-14 08:09:01 +0000

    Added expandable object support.

---
+++ SilverlightREPL/ExpanderButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
+using System.Windows.Input;
+
+namespace SilverlightREPL
+{
+    /// <summary>
+    /// Used to display an expander chevron and label.
+    /// </summary>
+    public class ExpanderButton : CheckBox
+    {
+        public ExpanderButton()
+        {
+            this.ClickMode = System.Windows.Controls.ClickMode.Press;
+            this.DefaultStyleKey = typeof(ExpanderButton);
+        }
+    }
+}
