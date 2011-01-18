6fa2b6300722ddd0334768fb69a9e6f6c1e3eac3
paulbartrum <paulbartrum@localhost>
2011-01-18 05:18:38 +0000

    Updated version numbers.

--- Jurassic/Properties/AssemblyInfo.cs
+++ Jurassic/Properties/AssemblyInfo.cs
@@ -28,7 +28,7 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("2.0.0.0")]
+[assembly: AssemblyVersion("2.1.0.0")]
 
 // The AllowPartiallyTrustedCallersAttribute requires the assembly to be signed with a strong name
 // key.
