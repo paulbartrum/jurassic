2b912d8c5f038ee362144933d0ebaa6650e63cb7
Paul Bartrum <paulbartrum@users.noreply.github.com>
2011-05-07 11:41:46 +1200

    Added additional GetMissingPropertyValue test.

--- Unit Tests/Core/ObjectExtensibilityTests.cs
+++ Unit Tests/Core/ObjectExtensibilityTests.cs
@@ -11,12 +11,11 @@ namespace UnitTests
     [TestClass]
     public class ObjectExtensibilityTests
     {
-        private class TestClass : ObjectInstance
+        private class TestClass1 : ObjectInstance
         {
-            public TestClass(ScriptEngine engine)
+            public TestClass1(ScriptEngine engine)
                 : base(engine, engine.Object.InstancePrototype)
             {
-                this.PopulateFunctions();
                 this.SetPropertyValue("exists", 1, false);
             }
 
@@ -26,13 +25,49 @@ namespace UnitTests
             }
         }
 
+        private class TestClass2 : ObjectInstance
+        {
+            public TestClass2(ScriptEngine engine)
+                : base(engine, engine.Object.InstancePrototype)
+            {
+            }
+
+            private class MyFunction : FunctionInstance
+            {
+                private string name;
+
+                public MyFunction(ScriptEngine engine, string name)
+                    : base(engine)
+                {
+                    this.name = name;
+                }
+
+                public override object CallLateBound(object thisObject, params object[] argumentValues)
+                {
+                    if ((thisObject is TestClass2) == false)
+                        throw new JavaScriptException(this.Engine, "TypeError", "Invalid 'this' value.");
+                    return ((TestClass2)thisObject).CallFunction(this.name, argumentValues);
+                }
+            }
+
+            protected override object GetMissingPropertyValue(string propertyName)
+            {
+                return new MyFunction(this.Engine, propertyName);
+            }
+
+            private object CallFunction(string functionName, params object[] argumentValues)
+            {
+                return functionName + argumentValues.Length;
+            }
+        }
+
         [TestMethod]
         public void GetMissingPropertyValue()
         {
             var engine = new ScriptEngine();
 
-            // Create a new TestClass instance.
-            engine.SetGlobalValue("test", new TestClass(engine));
+            // Create a new TestClass1 instance.
+            engine.SetGlobalValue("test", new TestClass1(engine));
 
             // Missing properties should return the name of the property.
             Assert.AreEqual("unknown", engine.Evaluate("test.unknown"));
@@ -50,6 +85,13 @@ namespace UnitTests
 
             // Missing properties return false from propertyIsEnumerable().
             Assert.AreEqual(false, engine.Evaluate("test.propertyIsEnumerable('unknown')"));
+
+            // Create a new TestClass2 instance.
+            engine.SetGlobalValue("test", new TestClass2(engine));
+
+            // Missing properties should return a function which can be called.
+            Assert.AreEqual("unknown0", engine.Evaluate("test.unknown()"));
+            Assert.AreEqual("unknown1", engine.Evaluate("test.unknown(1)"));
         }
     }
 }
