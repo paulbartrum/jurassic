0ae0be0e1c19652a7ec42aaada405fb1e3f78f08
Andrey Taritsyn <taritsyn@gmail.com>
2017-09-16 12:38:46 +0300

    Strong name signing for assembly

--- Jurassic/Properties/AssemblyInfo.cs
+++ Jurassic/Properties/AssemblyInfo.cs
@@ -1,4 +1,7 @@
 ﻿using System.Runtime.CompilerServices;
 
 // Unit tests and performance tests need access to internal members.
-[assembly: InternalsVisibleTo("Unit Tests")]
+[assembly: InternalsVisibleTo("Unit Tests, PublicKey=00240000048000009400000006020000002400005253" +
+    "413100040000010001000d9e3222b4ac5e04d6ea884cd918e62997c023dec31205991d186bb880b34edc455cb14b" +
+    "50c9751b42605c51c36b869df29b9f17ff2b5a44332ba06d52aebd58cf04a1366018cf14907ed9fc6e4ab979a791" +
+    "e283dfe1b5f55b1b3fb13bfa9456d7cfda968f442cadeb73586edc7594e3251965eb2ee14ad753355e9207d89ba4")]
