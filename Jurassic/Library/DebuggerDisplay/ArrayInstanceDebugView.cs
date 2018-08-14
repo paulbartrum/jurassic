c00829379b30efbcd896f97380b27a93d68e997c
objmalloc <atanas.kirilov@volley-labs.com>
2018-08-14 18:54:16 +0300

    DebuggerDisplay for ObjectInstane & ArrayInstance

---
+++ Jurassic/Library/DebuggerDisplay/ArrayInstanceDebugView.cs
@@ -0,0 +1,63 @@
+﻿using System.Diagnostics;
+using System.Linq;
+
+
+namespace Jurassic.Library
+{
+    /// <summary>
+    /// Debugger decorator for ArrayInstance.
+    /// </summary>
+    public class ArrayInstanceDebugView
+    {
+        /// <summary>
+        /// The watched array
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.Never)]
+        protected ArrayInstance arrayInstance;
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="arrayInstance">The watched object</param>
+        public ArrayInstanceDebugView(ArrayInstance arrayInstance)
+        {
+            this.arrayInstance = arrayInstance;
+        }
+
+        /// <summary>
+        /// The prototype reference
+        /// </summary>
+        public ObjectInstance __proto__
+        {
+            get
+            {
+                return this.arrayInstance.Prototype;
+            }
+        }
+
+
+        /// <summary>
+        /// Array properties list
+        /// </summary>
+        [DebuggerBrowsable(DebuggerBrowsableState.RootHidden)]
+        public object[] Properties
+        {
+            get
+            {
+                return this.arrayInstance.Properties.Where(
+                    pnv => (pnv.Key is string) && (pnv.Key as string) != "length").ToArray();
+            }
+        }
+
+        /// <summary>
+        /// The count of array elements
+        /// </summary>
+        public uint length
+        {
+            get
+            {
+                return this.arrayInstance.Length;
+            }
+        }
+    }
+}
---
+++ Jurassic/Library/DebuggerDisplay/DebuggerDisplayHelper.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+
+namespace Jurassic.Library
+{
+    /// <summary>
+    /// Helper methods used from debugger visualization
+    /// </summary>
+    public static class DebuggerDisplayHelper
+    {
+        /// <summary>
+        /// Converts any object to string, to be displayed in debugger watch window.
+        /// This is the long version, used when the object is directly displayed (not in parent).
+        /// </summary>
+        /// <param name="value">The object to be converted</param>
+        /// <returns>The result string</returns>
+        public static string StringRepresentation(object value)
+        {
+            string result;
+            if (value is ObjectInstance)
+            {
+                result = (value as ObjectInstance).DebuggerDisplayValue;
+            }
+            else if (value is string)
+            {
+                result = string.Format("\"{0}\"", value);
+            }
+            else
+            {
+                IFormattable formattable = value as IFormattable;
+                if (formattable == null)
+                {
+                    result = value.ToString();
+                }
+                else
+                {
+                    result = formattable.ToString(null, CultureInfo.InvariantCulture);
+                }
+            }
+            return result;
+        }
+
+
+        /// <summary>
+        /// Converts any object to string, to be displayed in debugger watch window.
+        /// This is the short version, used when the object is displayed as a child.
+        /// </summary>
+        /// <param name="value">The object to be converted</param>
+        /// <returns>The result string</returns>
+        public static string ShortStringRepresentation(object value)
+        {
+            string result;
+            if (value is ObjectInstance)
+            {
+                result = (value as ObjectInstance).DebuggerDisplayShortValue;
+            }
+            else if (value is string)
+            {
+                result = string.Format("\"{0}\"", value);
+            }
+            else
+            {
+                IFormattable formattable = value as IFormattable;
+                if (formattable == null)
+                {
+                    result = value.ToString();
+                }
+                else
+                {
+                    result = formattable.ToString(null, CultureInfo.InvariantCulture);
+                }
+            }
+            return result;
+        }
+    }
+}
